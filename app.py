fc6f857f56d4551699d821a069205c128f4cff02
dbraca11 <darwinbraca@gmail.com>
2026-01-18 00:59:20 -0400

    app.py

--- app.py
+++ app.py
@@ -1,20 +1,14 @@
 import time
-import random
 
 def monitor_system():
+    # Simulación de lo que haces en Bancaribe
     nodes = ["Servidor-Pagos", "API-Suiche7B", "Base-Datos-Oracle"]
-    print("--- INICIANDO MONITOREO DE ALTA DISPONIBILIDAD ---")
+    print("Iniciando sistema de monitoreo v1.0...")
     
-    while True:
-        for node in nodes:
-            # Simulamos que hay un 20% de probabilidad de que falle
-            if random.random() < 0.2:
-                print(f"[{time.ctime()}] ⚠️ ALERTA: {node} está CAÍDO ❌")
-            else:
-                print(f"[{time.ctime()}] {node} está OPERATIVO ✅")
-        
-        print("-" * 30)
-        time.sleep(5) # Espera 5 segundos para la próxima revisión
+    for node in nodes:
+        print(f"Revisando estado de: {node}...")
+        time.sleep(1)
+        print(f"Resultado: {node} está OPERATIVO ✅")
 
 if __name__ == "__main__":
     monitor_system()
